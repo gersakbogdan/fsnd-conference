7da5dfdd39e0d94895e3b571ccd493042bd0726c
Bogdan Gersak <bogdan@gersak.ro>
2015-11-11 16:33:29 +0000

    Remove asume-unchanged

--- static/js/app.js
+++ static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'replace with Web client ID',
+        CLIENT_ID: '54751868361-i018plbnbgq80kdro99rqk3qt12d07pk.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
